29d23ffe0d9e8cf0f075ec0efd0ba7c4bdea6e3c
Merve Dönmez <merve.donmez@ibtech.com.tr>
2023-03-04 18:50:12 +0300

    Initial Commit

---
+++ ThreadingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadingProject
+//
+//  Created by C02QJ4LQG8WN on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
